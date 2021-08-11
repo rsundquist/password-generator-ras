3423dd2018acf7544fe5a3ac3775a91e4ce7ac74
Rebecca Sundquist <rsundquist629@gmail.com>
2021-08-11 12:35:35 -0400

    updated console.logs for variables

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,48 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var alpha = "abcdefghijklmnopqrstuvwxyz";
-  var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric = "0123456789";
-  var special = "!$^&*-=+_?";
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   var alpha = "abcdefghijklmnopqrstuvwxyz";
+//   var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   var numeric = "0123456789";
+//   var special = "!$^&*-=+_?";
+
+  
     
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+function myRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomChar = arr[randomIndex];
+  return randomChar;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(myRandom([1,2,3,4,5,6,7,8,9,10]))
+
+console.log([1,2,3,4,5,6,7,8,9,10])
+
+console.log(myRandom(["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]));
+
+console.log(["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]);
+
+console.log(myRandom(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]));
+
+console.log(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]);
+
+console.log(myRandom(["!","$",",","^","&","*","-","=","+","_","?"]));
+
+console.log(["!","$",",","^","&","*","-","=","+","_","?"])
+
+
+
+
+
+
